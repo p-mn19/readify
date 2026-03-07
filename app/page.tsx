9d3fd4cc54c4b0fe52f94bf5afcffd4cf32edad1
p-mn19 <poornimamen2019@gmail.com>
2026-03-07 22:13:14 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,32 @@
+'use client';
 import * as React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
-
+const navItems=[
+  {label:"Library",href:"/"},
+  {label:"Add new",href:"/books/new"},
+]
 const Page = () => {
+  const pathName=usePathname();
   return ( 
-    <div className='wrapper navbar-height py-4 flex justify-between items-center'>
+    <div className="wrapper navbar-height py-4 flex justify-between items-center">
       <Link href="/" className="flex gap-0.5 items-center">
           <Image src="/assets/logo.png" alt="readify" width={42} height={26}/>
+          <span className="logo-text">Readify</span>
       </Link>
+      <nav className="w-fit flex gap-7.5 items-center">
+        {navItems.map(({label,href})=>{
+          const isActive=pathName==href || (href != '/' && pathName.startsWith(href));
+
+          return(
+            <Link href={href} key={label}>
+              {label}
+            </Link>
+          )
+        })}
+      </nav>
     </div>
    );
 }
